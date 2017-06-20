57aa7c39cb144181e6d74942c91c66e0cbe692a2
Aspen Hollyer <aspen.dollins@gmail.com>
2017-06-20 10:54:32 -0500

    Add basic server functionality

--- backend.js
+++ backend.js
@@ -1,37 +1,64 @@
+'use strict';
 const express = require('express');
 const app = express();
-
-/*************** Database ***************/
-const promise = require('bluebird');
-const pgp = require('pg-promise')({
-  promiseLib: promise,
-});
-const db = pgp({database: 'fooddev'});
+const body_parser = require('body-parser');
 
 /*********** API-Related Packages **********/
 const apicache = require('apicache');
 const cache = apicache.middleware;
 const axios = require('axios');
 
+/*************** Database ***************/
+const promise = require('bluebird');
+const pgp = require('pg-promise')({
+  promiseLib: promise,
+});
+const db = pgp({
+  host: 'localhost',
+  // NOTE: change to your preferred port for development --
+  // Must match your Postico settings
+  port: 9001,
+  database: 'fooddev',
+  user: 'postgres',
+  });
+
+// NOTE: Sample query: Un-Comment to check connection to database
+// db.query("SELECT * FROM restaurant")
+//   .then(function(results) {
+//     results.forEach(function(row) {
+//         console.log(row.name, row.atmosphere, row.parking, row.busy);
+//     });
+//     return db.one("SELECT * FROM restaurant WHERE name='tout suit'");
+//   })
 
 /************* Yelp API ****************/
 const yelp = require('yelp-fusion');
 const yelp_token = process.env.YELP_ACCESS_TOKEN;
 const yelp_client = yelp.client(yelp_token);
 
+// NOTE: Sample query: Un-Comment to check connection to Yelp-Fusion API
+// NOTE: Must have .env file with YELP_ACCESS_TOKEN to use API
+// yelp_client.search({
+//   term:'villa arcos',
+//   location: 'houston, tx'
+// }).then(response => {
+//   console.log(response.jsonBody.businesses[0]);
+// }).catch(err => {
+//   console.error(err);
+// });
 
+/*********** App Configuration **************/
+app.set('view engine', 'hbs');
+app.use('/static', express.static('static'));
 app.use('/axios', express.static('node_modules/axios/dist'));
+app.use(body_parser.urlencoded({extended: false}));
 
-yelp_client.search({
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
-}).then(response => {
-  console.log(response.jsonBody.businesses[0].name);
-}).catch(e => {
-  console.log(e);
+/************** Server *******************/
+app.get('/', function(req, resp) {
+  resp.render('index.hbs');
 });
 
-var PORT = process.env.PORT || 8000;
+let PORT = process.env.PORT || 9000;
 app.listen(PORT, function () {
   console.log('Listening on port ' + PORT);
 });
