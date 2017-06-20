5bf1ffe2de7e56ef9c24dab3cd0215a9f07410e3
Aspen Hollyer <aspen.dollins@gmail.com>
2017-06-20 14:12:14 -0500

    Set up basic search engine

--- backend.js
+++ backend.js
@@ -38,14 +38,14 @@ const yelp_client = yelp.client(yelp_token);
 
 // NOTE: Sample query: Un-Comment to check connection to Yelp-Fusion API
 // NOTE: Must have .env file with YELP_ACCESS_TOKEN to use API
-// yelp_client.search({
-//   term:'villa arcos',
-//   location: 'houston, tx'
-// }).then(response => {
-//   console.log(response.jsonBody.businesses[0]);
-// }).catch(err => {
-//   console.error(err);
-// });
+yelp_client.search({
+  term:'villa arcos',
+  location: 'houston, tx'
+}).then(response => {
+  console.log(response.jsonBody.businesses[0]);
+}).catch(err => {
+  console.error(err);
+});
 
 /*********** App Configuration **************/
 app.set('view engine', 'hbs');
@@ -54,8 +54,27 @@ app.use('/axios', express.static('node_modules/axios/dist'));
 app.use(body_parser.urlencoded({extended: false}));
 
 /************** Server *******************/
-app.get('/', function(req, resp) {
+app.get('/', function (req, resp) {
   resp.render('index.hbs');
+});
+
+/********* Search Engine ***********/
+// NOTE: Currently works only for restaurant names
+// Accepts GET parameters from search input and returns matching results
+// from database
+
+// Test in your dev environment:
+// localhost:9000/search?search_term=piola
+app.get('/search', function (req, resp, next) {
+  let term = req.query.search_term;
+  console.log(term);
+  let query = "SELECT * FROM restaurant WHERE restaurant.name ILIKE '%$1#%'";
+  db.any(query, term)
+    .then(function (results_array) {
+      console.log(results_array)
+      resp.render('search_results.hbs', {results: results_array});
+    })
+    .catch(next);
 });
 
 let PORT = process.env.PORT || 9000;
