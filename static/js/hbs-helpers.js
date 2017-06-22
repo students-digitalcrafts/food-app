eed332ffd2a3b8724f972297e8f1fcb4dbb7643a
Aspen Hollyer <aspen.dollins@gmail.com>
2017-06-22 12:00:12 -0500

    Create custom hbs helper function

--- static/js/hbs-helpers.js
+++ static/js/hbs-helpers.js
@@ -1,1 +1,10 @@
 /* Contains custom helper functions for handlebars templates */
+
+// Convert strings to title case
+// Handlebars.registerHelper("titleCase", function(str) {
+//   let words = str.split(' ');
+//
+//   return words.map(function(word) {
+//     return word[0].toUpperCase() + word.slice(1);
+//   }).join(' ')
+// });
