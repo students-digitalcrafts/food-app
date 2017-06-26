2a8d6093922a931621d267c8e1422b8810c064a6
fimanishi <fimanishi@gmail.com>
2017-06-26 15:58:00 -0500

    order by implemented for distance and rating

--- backend.js
+++ backend.js
@@ -308,7 +308,7 @@ app.get("/detail/", function(req, resp, next) {
 })
 
   /********* Calculate Distance ******/
-function degrees_to_radians(degrees)
+function toRadians(degrees)
 {
   var pi = Math.PI;
   return degrees * (pi/180);
@@ -316,17 +316,17 @@ function degrees_to_radians(degrees)
 
 function calculateDistance(lat1, lon1, lat2, lon2){
   var R = 6371e3; // metres
-  var φ1 = lat1.toRadians();
-  var φ2 = lat2.toRadians();
-  var Δφ = (lat2-lat1).toRadians();
-  var Δλ = (lon2-lon1).toRadians();
+  var φ1 = toRadians(lat1);
+  var φ2 = toRadians(lat2);
+  var Δφ = toRadians(lat2-lat1);
+  var Δλ = toRadians(lon2-lon1);
 
   var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
         Math.cos(φ1) * Math.cos(φ2) *
         Math.sin(Δλ/2) * Math.sin(Δλ/2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-  var d = R * c / 1.609344;
+  var d = R * c / 1609.34;
   return d
 }
 
@@ -334,10 +334,16 @@ function calculateDistance(lat1, lon1, lat2, lon2){
   /********* Order By ****************/
 
 app.post("/order_by/", function(request, response, next){
-  function order (category, session, body){
+  if(request.body.lat && request.body.long){
+    request.session["lat"] = request.body.lat;
+    request.session["long"] = request.body.long;
+  };
+  function order (category, session){
     if(category === "distance"){
       session.list.forEach(function(item){
-        session.list["distance"] = calculateDistance(session.list.lat, session.list.lon, body.lat, body.lon);
+        // console.log(session.list.latitude, session.list.longitude, body.lat, body.long);
+        item["distance"] = calculateDistance(item.latitude, item.longitude, session.lat, session.long);
+        console.log(item["name"] + ":" + item["distance"]);
       })
     }
     var ordered = [];
@@ -349,10 +355,19 @@ app.post("/order_by/", function(request, response, next){
       }
       else{
         for(var i=0; i<ordered.length; i++){
-          if(item[category] > ordered[i][category]){
-            ordered.splice(i, 0, item);
-            added = true;
-            break;
+          if(category === "rating"){
+            if(item[category] > ordered[i][category]){
+              ordered.splice(i, 0, item);
+              added = true;
+              break;
+            }
+          }
+          else if(category === "distance"){
+            if(item[category] < ordered[i][category]){
+              ordered.splice(i, 0, item);
+              added = true;
+              break;
+            }
           }
         }
       if(!added){
@@ -362,7 +377,7 @@ app.post("/order_by/", function(request, response, next){
     })
     return ordered
   }
-  var ordered = order(request.body.order, request.session, request.body);
+  var ordered = order(request.body.order, request.session);
   response.render('partials/list.hbs', {layout: false, results: ordered});
 })
 
